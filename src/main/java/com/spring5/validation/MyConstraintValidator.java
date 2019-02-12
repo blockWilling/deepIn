b78bcddfa936945e94cd1c95b16fbf1d6285ef9e
kangjin <358667556@qq.com>
2019-02-12 17:55:23 +0800

    2019-02-12

--- src/main/java/com/spring5/validation/MyConstraintValidator.java
+++ src/main/java/com/spring5/validation/MyConstraintValidator.java
@@ -1,5 +1,6 @@
 package com.spring5.validation;
 
+import com.spring5.anno.MyConstraint;
 import com.spring5.entity.Person;
 
 import javax.validation.ConstraintValidator;
@@ -9,15 +10,15 @@ import java.lang.annotation.Annotation;
 /**
  * Created by 康金 on 2019/2/12.
  */
-public class MyConstraintValidator implements ConstraintValidator {
+public class MyConstraintValidator implements ConstraintValidator<MyConstraint,String> {
+
     @Override
-    public void initialize(Annotation constraintAnnotation) {
+    public void initialize(MyConstraint constraintAnnotation) {
 
     }
 
     @Override
-    public boolean isValid(Object value, ConstraintValidatorContext context) {
-
-        return true;
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return false;
     }
 }
